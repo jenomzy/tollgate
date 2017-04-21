75a5847ae88276e778ef07a33f7c5d87c2c4fcb5
jenomzy <jenomzy25@gmail.com>
2017-04-21 10:09:20 +0000

    allowed cross origin

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ var app = express();
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'null');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
